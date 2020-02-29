fea93c885dc97954102159ab80d2f3bea1ce5dff
Salman Zafar <maanizfar@gmail.com>
2020-02-29 18:39:25 +0500

    refactor

--- coin-flipper/flip.js
+++ coin-flipper/flip.js
@@ -11,37 +11,36 @@ const args = parseArgs(process.argv.slice(2), {
 if (args.help) {
   printHelp();
 } else if (process.argv.length == 2) {
-  console.log(flipCoin());
+  flipCoins();
 } else if (process.argv.length == 3 && typeof args._[0] === "number") {
   if (args._[0] === 1) {
-    console.log(flipCoin());
+    flipCoins(1);
   } else {
-    flipnCoins(args._[0]);
+    flipCoins(args._[0]);
   }
 } else {
   printError("Incorrect usage.", true);
 }
 
-function flipCoin() {
-  let rand = Math.random();
-  let output = "";
-  if (rand < 0.5) {
-    output = "Head";
-  } else {
-    output = "Tails";
-  }
-  return output;
-}
+function flipCoins(numOfTimes = 1) {
+  const result = { heads: 0, tails: 0, outcomes: [] };
 
-function flipnCoins(n) {
-  let heads = 0;
-  let tails = 0;
-  for (let i = 0; i < n; i++) {
-    if (flipCoin() === "Head") heads++;
-    else tails++;
+  for (let i = 0; i < numOfTimes; i++) {
+    let rand = Math.random();
+    if (rand < 0.5) {
+      result.heads++;
+      result.outcomes.push("Heads");
+    } else {
+      result.tails++;
+      result.outcomes.push("Tails");
+    }
   }
-  console.log("Heads: ", heads);
-  console.log("Tails: ", tails);
+
+  if (numOfTimes == 1) console.log(result.outcomes[0]);
+  else {
+    console.log("Heads: ", result.heads);
+    console.log("Tails: ", result.tails);
+  }
 }
 
 function printError(msg, showHelp = false) {
